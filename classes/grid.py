dc5f644339f860bdc349ab4b1089f33adc99cb8e
Hugo-Gomez <hugo.gomez9795@gmail.com>
2020-04-06 20:57:04 +0200

    optimization & grid display style

--- classes/grid.py
+++ classes/grid.py
@@ -19,7 +19,7 @@ class Grid:
             self.grid = []
             # grid full of 0
             for _ in range_grid_size:
-                row = [0 for _ in range_grid_size]
+                row = [Square(0) for _ in range_grid_size]
                 self.grid.append(row)
             # Replacing all the 0 by good values
             for r in range_grid_size:
@@ -31,15 +31,15 @@ class Grid:
                         must_generate = True
                         break
                     for c in range_grid_size:
-                        self.grid[r][c] = 0
+                        self.grid[r][c].value = 0
                         int_available = [i for i in range(1, 10)]
-                        while self.grid[r][c] == 0:
+                        while self.grid[r][c].value == 0:
                             random_int = random.choice(int_available)
                             coordinates = (r, c)
                             is_not_in_col = self.check_column(coordinates, random_int)
                             is_not_in_square_fam = self.check_square_family(coordinates, random_int)
-                            if random_int not in self.grid[r] and is_not_in_col and is_not_in_square_fam:
-                                self.grid[r][c] = random_int
+                            if random_int not in [val.value for val in self.grid[r]] and is_not_in_col and is_not_in_square_fam:
+                                self.grid[r][c] = Square(random_int)
                             else:
                                 int_available.remove(random_int)
                             if len(int_available) == 0:
@@ -56,12 +56,12 @@ class Grid:
         prev_rows = range(0, row)
         next_rows = range(row, 9)
         for row_pos in prev_rows:
-            if self.grid[row_pos][col] == randint:
+            if self.grid[row_pos][col].value == randint:
                 same_number = False
         if not same_number:
             return same_number
         for row_pos in next_rows:
-            if self.grid[row_pos][col] == randint:
+            if self.grid[row_pos][col].value == randint:
                 same_number = False
         return same_number
 
@@ -71,7 +71,7 @@ class Grid:
             if coordinates in square:
                 related_square = square
         related_square.remove(coordinates)
-        related_square_values = [self.grid[r][c] for r, c in related_square]
+        related_square_values = [self.grid[r][c].value for r, c in related_square]
         if randint in related_square_values:
             return False
         else:
@@ -87,12 +87,13 @@ class Grid:
             s2 = []
             s3 = []
             for r in range(r_min, r_max):
-                for c in range(0, 3):
-                    s1.append((r, c))
-                for c in range(3, 6):
-                    s2.append((r, c))
-                for c in range(6, 9):
-                    s3.append((r, c))
+                c_min, c_max = 0, 3
+                for i in range(1, 4):
+                    for c in range(c_min, c_max):
+                        correct_list = locals()["s" + str(i)]
+                        correct_list.append((r,c))
+                    c_min += 3
+                    c_max += 3
             squares.append(s1)
             squares.append(s2)
             squares.append(s3)
@@ -101,7 +102,16 @@ class Grid:
 
         return squares
 
-
+    # TODO : Hide and show some values according to the level specified on grid instanciation
     def print_grid(self):
+        row_count = 0
+        print("╔═══╤═══╤═══╦═══╤═══╤═══╦═══╤═══╤═══╗")
         for row in self.grid:
-            print(f"{row}")
+            displayable_list = [str(val.value) for val in row]
+            print("║ " + ' | '.join(displayable_list[:3]) + " ║ " +' | '.join(displayable_list[3:6]) + " ║ " +' | '.join(displayable_list[6:9]) + " ║")
+            if row_count in [2, 5]:
+                print("║═══╪═══╪═══║═══╪═══╪═══║═══╪═══╪═══║")
+            elif row_count != 8:
+                print("║───┼───┼───║───┼───┼───║───┼───┼───║")
+            row_count += 1
+        print("╚═══╧═══╧═══╩═══╧═══╧═══╩═══╧═══╧═══╝")
